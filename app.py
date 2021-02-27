2126e65319e496efe58edc8d8cb4639b255e814d
yarmout <yarmout@gmail.com>
2021-02-27 16:49:32 +0000

    Add work from hw10

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 import os
-from flask import Flask, send_from_directory, json
+from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='./build/static')
 
-# Point SQLAlchemy to your Heroku database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-# Gets rid of a warning
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-# IMPORTANT: This must be AFTER creating db variable to prevent
-# circular import issues
-from models import Person
-
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 socketio = SocketIO(
     app,
     cors_allowed_origins="*",
@@ -42,18 +31,16 @@ def on_disconnect():
 
 # When a client emits the event 'chat' to the server, this function is run
 # 'chat' is a custom event name that we just decided
-@socketio.on('chat')
+@socketio.on('move')
 def on_chat(data): # data is whatever arg you pass in your emit call on client
     print(str(data))
     # This emits the 'chat' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
-    socketio.emit('chat',  data, broadcast=True, include_self=False)
+    socketio.emit('move',  data, broadcast=True, include_self=False)
 
-# Note we need to add this line so we can import app in the python shell
-if __name__ == "__main__":
 # Note that we don't call app.run anymore. We call socketio.run with app arg
-    socketio.run(
-        app,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-    )
+socketio.run(
+    app,
+    host=os.getenv('IP', '0.0.0.0'),
+    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+)
