853d33aca6fb4739f59a90d34d464a3fba86a10e
yarmout <yarmout@gmail.com>
2021-03-01 21:25:23 +0000

    Add Live users list. Add game logic and win condition

--- src/App.js
+++ src/App.js
@@ -1,56 +1,144 @@
-import logo from './logo.svg';
-import './App.css';
-import './Board.css';
-import { Board } from './Board.js';
-import { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-
-
+import "./App.css";
+import "./Board.css";
+import { Board } from "./Board.js";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { ListItem } from "./ListItem.js";
 
 const socket = io();
 
 function App() {
-  const [board, setBoard] = useState([null, null, null, null, null, null, null, null, null]);
+  const [board, setBoard] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
   const [isLoggedIn, setLogin] = useState(false);
   const [username, setUsername] = useState("");
   const [userList, setUserList] = useState([null, null]);
-  
+
   function onClickSquare(square_index) {
-      socket.emit('move', square_index);
-      setBoard(prevBoard => {
-          const newBoard = [...prevBoard];
-          if (username === userList[0]) {
-            newBoard[square_index] = 'X';
-          }
-          else if (username === userList[1]) {
-            newBoard[square_index] = 'O';
-          }
-          return newBoard;
-      });
+    socket.emit("move", square_index);
+    setBoard((prevBoard) => {
+      const newBoard = [...prevBoard];
+      if (username === userList[0]) {
+        newBoard[square_index] = "X";
+      } else if (username === userList[1]) {
+        newBoard[square_index] = "O";
+      }
+      return newBoard;
+    });
   }
-  
+
+  function winCondition(tileLetter) {
+    if (
+      board[6] === tileLetter &&
+      board[7] === tileLetter &&
+      board[8] === tileLetter
+    ) {
+      return true;
+    }
+
+    if (
+      board[3] === tileLetter &&
+      board[4] === tileLetter &&
+      board[5] === tileLetter
+    ) {
+      return true;
+    }
+
+    if (
+      board[0] === tileLetter &&
+      board[1] === tileLetter &&
+      board[2] === tileLetter
+    ) {
+      return true;
+    }
+
+    if (
+      board[6] === tileLetter &&
+      board[3] === tileLetter &&
+      board[0] === tileLetter
+    ) {
+      return true;
+    }
+
+    if (
+      board[7] === tileLetter &&
+      board[4] === tileLetter &&
+      board[1] === tileLetter
+    ) {
+      return true;
+    }
+
+    if (
+      board[8] === tileLetter &&
+      board[5] === tileLetter &&
+      board[2] === tileLetter
+    ) {
+      return true;
+    }
+
+    if (
+      board[6] === tileLetter &&
+      board[4] === tileLetter &&
+      board[2] === tileLetter
+    ) {
+      return true;
+    }
+
+    if (
+      board[8] === tileLetter &&
+      board[4] === tileLetter &&
+      board[0] === tileLetter
+    ) {
+      return true;
+    }
+
+    if (tileLetter === "draw") {
+      var draw = null;
+      for (var i = 0; i < board.length; i++) {
+        if (board[i] === null) {
+          draw = false;
+        }
+      }
+      if (draw != false) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   function onLogin() {
-    console.log('Username:');
-    console.log(username);
-    socket.emit('login', username);
-    setLogin(previsLoggedIn => {
+    socket.emit("login", username);
+    setLogin((previsLoggedIn) => {
       return true;
     });
   }
-  
+
+  function getSpectators() {
+    if (userList.length > 2) {
+      return userList.slice(2);
+    }
+    return [];
+  }
+
   useEffect(() => {
-    // Listening for a chat event emitted by the server. If received, we
-    // run the code in the function that is passed in as the second arg
-    socket.on('userList', (data) => {
-      console.log('User list received!');
-      console.log(data);
+    socket.on("userList", (data) => {
       var userListLengthTracker = userList.length;
-      while (data.length > userListLengthTracker){
-            setUserList(prevList => [...prevList, null]);
-            userListLengthTracker = userListLengthTracker + 1;
-          }
-          
-      setUserList(prevList => {
+      while (data.length > userListLengthTracker) {
+        setUserList((prevList) => [...prevList, null]);
+        userListLengthTracker = userListLengthTracker + 1;
+      }
+
+      setUserList((prevList) => {
         const newList = [...prevList];
         for (var i = 0; i < data.length; i++) {
           newList[i] = data[i];
@@ -59,54 +147,73 @@ function App() {
       });
     });
   }, []);
-  
+
   useEffect(() => {
-    // Listening for a chat event emitted by the server. If received, we
-    // run the code in the function that is passed in as the second arg
-    socket.on('move', (data) => {
-      console.log('Player move received!');
-      console.log(data);
-      // If the server sends a message (on behalf of another client), then we
-      // add it to the list of messages to render it on the UI.
-      setBoard(prevBoard => {
-          const newBoard = [...prevBoard];
-          
-          if (username === userList[0]) {
-            newBoard[data] = 'O';
-          }
-          
-          else if (username === userList[1]) {
-            newBoard[data] = 'X';
-          }
-          
-          return newBoard;
+    console.log('Board updated');
+    if (winCondition("X")) {
+      if (window.confirm("Game over! " + userList[0] + " wins! Play again?")) {
+        window.location.reload();
+      }
+    } else if (winCondition("O")) {
+      if (window.confirm("Game over! " + userList[1] + " wins! Play again?")) {
+        window.location.reload();
+      }
+    } else if (winCondition("draw")) {
+      if (window.confirm("Game over! Draw! Play again?")) {
+        window.location.reload();
+      }
+    }
+  }, [board]);
+
+  useEffect(() => {
+    socket.on("move", (data) => {
+      setBoard((prevBoard) => {
+        const newBoard = [...prevBoard];
+
+        if (username === userList[0]) {
+          newBoard[data] = "O";
+        } else if (username === userList[1]) {
+          newBoard[data] = "X";
+        }
+
+        return newBoard;
       });
     });
   }, [username, userList]);
   
-  
   return (
+  <div>
+    {isLoggedIn === false ?(
     <div>
-      {isLoggedIn === false ?(
-        <div>
-          <form>
+        <form>
             <label>
             Please enter your username <br></br>
             <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
             <button onClick={() => {onLogin()}}> Login </button>
             </label>
-          </form>
-        </div>
-      ) : (
-        <div>
-          <p> Welcome {username}! </p>
-          <div class="board">
-            <Board onClick={onClickSquare} board={board} />
-          </div>
-        </div>
-      )}
+        </form>
     </div>
+    ) : (
+    <div>
+        <p> Welcome {username}! </p>
+        <p> Current active users: </p>
+        <p> Player X: </p>
+        <ListItem name={userList[0]} />
+        <p> Player O: </p>
+        <ListItem name={userList[1]} />
+        <p> Spectator(s): </p>
+        {getSpectators().map(user => 
+        <ListItem name={user} />
+        )}
+        <div class="board">
+            <Board onClick={onClickSquare} board={board} />
+        </div>
+    </div>
+    )}
+  </div>
   );
 }
 
 export default App;
+
+
