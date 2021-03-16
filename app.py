e2c588fe1d46f514ff1ab2a66c0d05408c87613a
yarmout <yarmout@gmail.com>
2021-03-16 00:12:08 +0000

    pylint and yapf checks

--- app.py
+++ app.py
@@ -1,96 +1,100 @@
+"""Python backend that communicates with javascript frontend and postgresql database"""
 import os
-from flask import Flask, send_from_directory, json, session
+from flask import Flask, send_from_directory, json
 from flask_socketio import SocketIO
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
+from models import DB, Player
 
 load_dotenv(find_dotenv())
 
-app = Flask(__name__, static_folder='./build/static')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+APP = Flask(__name__, static_folder='./build/static')
+APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-import models
+DB.init_app(APP)
 
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+CORS = CORS(APP, resources={r"/*": {"origins": "*"}})
 
-socketio = SocketIO(app,
-                    cors_allowed_origins="*",
-                    json=json,
-                    manage_session=False)
+SOCKET_IO = SocketIO(APP,
+                     cors_allowed_origins="*",
+                     json=json,
+                     manage_session=False)
 
 
-@app.route('/', defaults={"filename": "index.html"})
-@app.route('/<path:filename>')
+@APP.route('/', defaults={"filename": "index.html"})
+@APP.route('/<path:filename>')
 def index(filename):
+    """Built-in React function"""
     return send_from_directory('./build', filename)
 
 
-@socketio.on('login')
+@SOCKET_IO.on('login')
 def on_login(data):
-    from models import Player
+    """If a new user logs in, adds them to the database"""
     all_players = []
-    for player in db.session.query(Player).all():
+    for player in DB.session.query(Player).all():
         all_players.append(player.username)
     if data['currentUser'] not in all_players:
-        currentUser = Player(username=data['currentUser'], score=100)
-        db.session.add(currentUser)
-        db.session.commit()
+        current_user = Player(username=data['currentUser'], score=100)
+        DB.session.add(current_user)
+        DB.session.commit()
 
     leaderboard = {'players': [], 'scores': []}
-    for player in db.session.query(Player).order_by(Player.score.desc()).all():
+    for player in DB.session.query(Player).order_by(Player.score.desc()).all():
         leaderboard['players'].append(player.username)
         leaderboard['scores'].append(player.score)
 
-    socketio.emit('updateLeaderboard',
-                  leaderboard,
-                  broadcast=True,
-                  include_self=True)
-    socketio.emit('login', data, broadcast=True, include_self=True)
+    SOCKET_IO.emit('updateLeaderboard',
+                   leaderboard,
+                   broadcast=True,
+                   include_self=True)
+    SOCKET_IO.emit('login', data, broadcast=True, include_self=True)
 
 
-@socketio.on('move')
+@SOCKET_IO.on('move')
 def on_move(data):
-    socketio.emit('move', data, broadcast=True, include_self=True)
+    """Upon receiving an updated board state, emit it to all clients"""
+    SOCKET_IO.emit('move', data, broadcast=True, include_self=True)
 
 
-@socketio.on('gameOver')
-def on_gameOver(data):
-    from models import Player
+@SOCKET_IO.on('gameOver')
+def on_game_over(data):
+    """Upon receiving the gameOver emit, updates the players scores in the database and emits
+     the updated leaderboard and gameOver signal to all clients"""
     print('Game over emit received')
     if data['winner'] != 'Draw!':
-        winner = db.session.query(Player).filter_by(
+        winner = DB.session.query(Player).filter_by(
             username=data['winner']).first()
-        loser = db.session.query(Player).filter_by(
+        loser = DB.session.query(Player).filter_by(
             username=data['loser']).first()
         winner.score = winner.score + 1
         loser.score = loser.score - 1
         print(winner.score)
         print(loser.score)
-        db.session.commit()
+        DB.session.commit()
 
     leaderboard = {'players': [], 'scores': []}
-    for player in db.session.query(Player).order_by(Player.score.desc()).all():
+    for player in DB.session.query(Player).order_by(Player.score.desc()).all():
         leaderboard['players'].append(player.username)
         leaderboard['scores'].append(player.score)
 
-    socketio.emit('updateLeaderboard',
-                  leaderboard,
-                  broadcast=True,
-                  include_self=True)
-    socketio.emit('gameOver', data, broadcast=True, include_self=True)
+    SOCKET_IO.emit('updateLeaderboard',
+                   leaderboard,
+                   broadcast=True,
+                   include_self=True)
+    SOCKET_IO.emit('gameOver', data, broadcast=True, include_self=True)
 
 
-@socketio.on('restart')
+@SOCKET_IO.on('restart')
 def on_restart(data):
-    socketio.emit('restart', data, broadcast=True, include_self=True)
+    """Upon receiving the restart emit, emits the restart signal to all clients"""
+    SOCKET_IO.emit('restart', data, broadcast=True, include_self=True)
 
 
 if __name__ == "__main__":
-    socketio.run(
-        app,
+    SOCKET_IO.run(
+        APP,
         debug=True,
         host=os.getenv('IP', '0.0.0.0'),
         port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
--- models.py
+++ models.py
@@ -1,10 +1,14 @@
-from app import db
+"""Creates the database schema and initializes the database to be imported into app.py"""
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
 
 
-class Player(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    score = db.Column(db.Integer, unique=False, nullable=False)
+class Player(DB.Model):
+    """Database config for the leaderboard"""
+    id = DB.Column(DB.Integer, primary_key=True)
+    username = DB.Column(DB.String(80), unique=True, nullable=False)
+    score = DB.Column(DB.Integer, unique=False, nullable=False)
 
     def __repr__(self):
         return '<Player %r>' % self.username
